fbb51b704af99352e2b565b951a72792425f701a
nar17 <122194228+nar17@users.noreply.github.com>
2023-10-09 12:42:08 -0400

    successfully scraped quotes and other items as directed from geeksforgeeks

--- geeksforgeeksScrapeTest.py
+++ geeksforgeeksScrapeTest.py
@@ -1,8 +1,29 @@
+#https://www.geeksforgeeks.org/implementing-web-scraping-python-beautiful-soup/
+#10-2023 ish
+
 import requests
 from bs4 import BeautifulSoup
+import csv
 
-URL = "https://www.geeksforgeeks.org/data-structures/"
+URL = "http://www.values.com/inspirational-quotes"
 r = requests.get(URL)
-
 soup = BeautifulSoup(r.content, 'html5lib')
-print(soup.prettify())
+
+quotes = []
+table = soup.find('div', attrs = {'id':'all_quotes'})
+
+for row in table.findAll('div', attrs = {'class':'col-6 col-lg-4 text-center margin-30px-bottom sm-margin-30px-top'}):
+	quote = {}
+	quote['theme'] = row.h5.text
+	quote['url'] = row.a['href']
+	quote['img'] = row.img['src']
+	quote['lines'] = row.img['alt'].split(" #")[0]
+	quote['author'] = row.img['alt'].split(" #")[1]
+	quotes.append(quote)
+
+filename = 'inspirational-quotes.csv'
+with open(filename, 'w', newline='') as f:
+	w = csv.DictWriter(f,['theme','url','img','lines','author'])
+	w.writeheader()
+	for quote in quotes:
+		w.writerow(quote)
